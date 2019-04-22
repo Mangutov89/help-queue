930065648cc6fba92c7f64f0e9ed590271ea7d2e
Student <example@example.com>
2019-04-22 11:34:49 -0700

    add forms and refs

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
 
 class NewTicketControl extends React.Component {
 
