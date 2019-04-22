c17c26decf695e5b26c7fe529d6a4dc35b171880
Student <example@example.com>
2019-04-22 15:37:54 -0700

    add UUID to component

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -8,11 +8,11 @@ function TicketList(props) {
   return (
     <div>
       <hr/>
-      {props.ticketList.map((ticket, index) =>
+      {props.ticketList.map((ticket) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          key={index}/>
+          key={ticket.id}/>
       )}
     </div>
   );
